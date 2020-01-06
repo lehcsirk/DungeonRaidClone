d10f1797524e878fad7af95019e39ecc1b53f6cf
Cameron Krischel <ckrischel@he3labs.com>
2020-01-06 14:23:12 -0800

    Initial Commit

---
+++ DungeonRaidClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DungeonRaidClone
+//
+//  Created by Cameron Krischel on 1/6/20.
+//  Copyright © 2020 Cameron Krischel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
